54da20b65f93509d2a861e2f4278299a473f93eb
Maxim Alekseev <max.alek@truelabel.io>
2025-02-25 10:54:23 +0300

    feat: pagination

--- backend/src/exercise/exercise.controller.ts
+++ backend/src/exercise/exercise.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Query,
+} from '@nestjs/common';
 import { Exercise } from './exercise.entity';
 import { ExerciseService } from './exercise.service';
 
@@ -7,7 +14,29 @@ export class ExerciseController {
   constructor(private exerciseService: ExerciseService) {}
 
   @Get()
-  getExercises(): Promise<Exercise[]> {
-    return this.exerciseService.getExercises();
+  getExercises(
+    @Query('page') page = 1,
+    @Query('limit') limit = 10,
+  ): Promise<{ items: Exercise[]; total: number; pagesCount: number }> {
+    if (isNaN(page) || page <= 0) {
+      throw new BadRequestException('Неккоректный query параметр: page');
+    }
+
+    if (isNaN(limit) || limit <= 0) {
+      throw new BadRequestException('Неккоректный query параметр: limit');
+    }
+
+    return this.exerciseService.getExercises(page, limit);
+  }
+
+  @Get(':id')
+  async getExerciseById(@Param('id') id: number): Promise<Exercise | null> {
+    const exercise = await this.exerciseService.getExerciseById(id);
+
+    if (!exercise) {
+      throw new NotFoundException(`Упражнение не найдено`);
+    }
+
+    return exercise;
   }
 }
--- backend/src/exercise/exercise.service.ts
+++ backend/src/exercise/exercise.service.ts
@@ -9,7 +9,19 @@ export class ExerciseService {
     private exerciseRepository: Repository<Exercise>,
   ) {}
 
-  async getExercises(): Promise<Exercise[]> {
-    return this.exerciseRepository.find();
+  async getExercises(
+    page: number,
+    limit: number,
+  ): Promise<{ items: Exercise[]; total: number; pagesCount: number }> {
+    const [items, total] = await this.exerciseRepository.findAndCount({
+      take: limit,
+      skip: (page - 1) * limit,
+    });
+
+    return { items, total, pagesCount: Math.ceil(total / limit) };
+  }
+
+  async getExerciseById(id: number): Promise<Exercise | null> {
+    return this.exerciseRepository.findOne({ where: { id } });
   }
 }
