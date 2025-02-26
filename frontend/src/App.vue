316ea2e908d6a888b0fe88464471f055f6bddf53
Maxim Alekseev <max.alek@truelabel.io>
2025-02-26 15:40:02 +0300

    feat(frantend): any margins

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@ import { ExercisesList } from '@/components/widgets';
 </script>
 
 <template>
-  <div class="mx-auto max-w-[500px]">
+  <div class="mx-4 max-w-[500px] sm:mx-auto">
     <h1>Упражнения</h1>
 
     <p class="mb-4 opacity-50">Здесь можно найти упражнения для развития необходимых вам групп мышц</p>
